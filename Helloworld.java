b5f071e6cf5cfc3a89e0e32002a0c0177935bc1a
Mohit Tomar <onewebdev@outlook.com>
2022-05-30 13:26:27 +0530

    hello world added

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+/**
+ * Helloworld
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        int i = 5;
+        if (i == 5) {
+            System.out.println(s);
+        } else {
+            System.out.println("not hello world");
+        }
+    }
+}
